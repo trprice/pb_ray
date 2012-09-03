72eaedbdecb00f93e8f3fa366b2985e1f28ce33e
Taylor Price <trprice@gmail.com>
2012-09-03 12:13:51 -0700

    Initial commit of pb_ray!

---
+++ core_tests/core_tests.cpp
@@ -0,0 +1,15 @@
+/*
+ *  core_tests.cpp
+ *  p_ray
+ *
+ *  Created by Taylor Price on 8/9/12.
+ *  Copyright 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "core_tests.h"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
