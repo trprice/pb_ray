b39602ca327ecfc7e58a9c5bf6eb5180fa55efe1
Taylor Price <trprice@gmail.com>
2013-04-29 17:10:23 -0700

    Add the MIT License Info and vim autocmds to add the header and update the timestamp

--- src/core_tests/core_tests.h
+++ src/core_tests/core_tests.h
@@ -1,10 +1,31 @@
 /*
- *  core_tests.h
- *  p_ray
+ *	pb_ray source code
+ *	
+ *	This file is part of pb_ray.
+ *	
+ *	pb_ray is free software; you can redistribute it and/or modify
+ *	it under the terms of The MIT License (opensource.org/licenses/MIT).
+ *	
+ *	pb_ray is based the book "Physically Based Rendering" written by Matt Pharr
+ *	and Greg Humphreys. The book and its contents are *not* licensed under
+ *	The MIT License.
+ *	
+ *	pb_ray is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of The MIT License along with this program.
+ *	If not, see <http://www.opensource.org/licenses/MIT>
  *
- *  Created by Taylor Price on 8/9/12.
- *  Copyright 2012 __MyCompanyName__. All rights reserved.
  *
+ *  File Name: core_tests.h
+ *
+ *  Purpose:
+ *
+ *  Creation Date: 8/9/12
+ *
+ *  Last Modified: Mon 29 Apr 2013 05:07:36 PM PDT
  */
 
 #include "gtest/gtest.h"
